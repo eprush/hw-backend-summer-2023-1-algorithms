4b79af39df03f65cf021f2268765f053a1ec402e
eprush <pavlovich.egorka@list.ru>
2023-02-06 22:07:49 +0300

    solve text without 2 tests

--- tasks/text.py
+++ tasks/text.py
@@ -10,4 +10,16 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    raise NotImplementedError
+    if text == "":
+        return None, None
+
+    arr = text.split()
+    min_word, max_word = arr[0], arr[0]
+    for word in arr:
+        if len(word) > len(max_word):
+            max_word = word
+        elif len(word) < len(min_word):
+            min_word = word
+    if min_word == max_word:
+        return None, None
+    return min_word, max_word
