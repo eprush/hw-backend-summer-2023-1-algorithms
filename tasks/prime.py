3254e0084cd03bb3c1fbaaf36912bb0b99cb68da
eprush <pavlovich.egorka@list.ru>
2023-02-07 17:58:08 +0300

    solve prime

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,4 +7,10 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if (not number) or (number == 1):
+        return False
+
+    for i in range(2, int(number**0.5)+1):
+        if not number%i:
+            return False
+    return True
